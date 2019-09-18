6a601358eb04421c40dd7321fb668c6a982b7864
Isaiah Brashears <isaiah.brashears13@gmail.com>
2019-09-18 16:16:55 -0500

    uncommented out code

--- src/BonusJavaString.java
+++ src/BonusJavaString.java
@@ -13,60 +13,60 @@ public class BonusJavaString {
 //        input - 12/01/1999
 //        output - December 12, 1999
 
-//        System.out.println("enter birthday in this format: MM/DD/YYYY ");
-//        String date = input.nextLine();
-//
-//        String month = date.substring(0,2);
-//        String dayYear = date.substring(3,5) + ", " + date.substring(6);
-//        System.out.println(month);
+        System.out.println("enter birthday in this format: MM/DD/YYYY ");
+        String date = input.nextLine();
+
+        String month = date.substring(0,2);
+        String dayYear = date.substring(3,5) + ", " + date.substring(6);
+        System.out.println(month);
 
 
-//        if(month.equals( "01")){
-//            month = "January ";
-//
-//        }else if(month.equals( "02")){
-//            month = "February ";
-//
-//        }else if(month.equals( "03")){
-//            month = "March ";
-//
-//        }else if(month.equals( "04")){
-//            month = "April ";
-//
-//        }else if(month.equals("05")){
-//            month = "May ";
-//
-//        }else if(month.equals("06")){
-//            month = "June ";
-//
-//        }else if(month.equals( "07")){
-//            month = "July ";
-//
-//        }else if(month.equals("08")){
-//            month = "August ";
-//
-//        }else if(month.equals("09")){
-//            month = "September ";
-//
-//        }else if(month.equals("10")){
-//            month = "October ";
-//
-//        }else if(month.equals("11")){
-//            month = "November ";
-//
-//        }else if(month.equals("12")){
-//            month = "December ";
-//        }
-//
-//        System.out.println(month + dayYear);
+        if(month.equals( "01")){
+            month = "January ";
+
+        }else if(month.equals( "02")){
+            month = "February ";
+
+        }else if(month.equals( "03")){
+            month = "March ";
+
+        }else if(month.equals( "04")){
+            month = "April ";
+
+        }else if(month.equals("05")){
+            month = "May ";
+
+        }else if(month.equals("06")){
+            month = "June ";
+
+        }else if(month.equals( "07")){
+            month = "July ";
+
+        }else if(month.equals("08")){
+            month = "August ";
+
+        }else if(month.equals("09")){
+            month = "September ";
+
+        }else if(month.equals("10")){
+            month = "October ";
+
+        }else if(month.equals("11")){
+            month = "November ";
+
+        }else if(month.equals("12")){
+            month = "December ";
+        }
+
+        System.out.println(month + dayYear);
 
 
 
-//        Create an application that allows the user to enter a sentence
-//        and tells them how many vowels and consonants were used.
-//        Example:
-//        input - "The hill are alive."
-//        output - 5 vowels and 10 consonants
+        Create an application that allows the user to enter a sentence
+        and tells them how many vowels and consonants were used.
+        Example:
+        input - "The hill are alive."
+        output - 5 vowels and 10 consonants
 
         System.out.println("enter a sentence: ");
         String userSentence = input.nextLine();
